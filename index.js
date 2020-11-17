8e9746ab02e7454ad70f659624e01ae3f36de942
Hendrik Ulbrich <Hendrik.Ulbrich@de.ibm.com>
2020-11-17 11:37:17 +0100

    Fix the fix

--- index.js
+++ index.js
@@ -8,19 +8,23 @@ const childProcess = require("child_process")
 
 const deps = packageJson.envDependencies || {}
 
-const envPackages = Object.keys(deps)
-	.map(key =>
-		deps[key].replace(/\${([0-9a-zA-Z_]*)}/g, x => {
-			return { key, value: process.env[x.substring(2, x.length - 1)] }
+const envPackages = Object.keys(deps).map(key => {
+	return {
+		key,
+		value: deps[key].replace(/\${([0-9a-zA-Z_]*)}/g, x => {
+			return process.env[x.substring(2, x.length - 1)]
 		})
-	)
+	}
+})
 const envValues = envPackages.map(pkg => pkg.value).join(" ")
 const envKeys = envPackages.map(pkg => pkg.key).join(" ")
 
-
 try {
-	childProcess.execSync(`NOYARNPOSTINSTALL=1 yarn add --dev ${envValues} && yarn remove ${envValues}`, {
-		stdio: [0, 1, 2]
-	})
+	childProcess.execSync(
+		`NOYARNPOSTINSTALL=1 yarn add --dev ${envValues} && yarn remove ${envKeys}`,
+		{
+			stdio: [0, 1, 2]
+		}
+	)
 	// eslint-disable-next-line no-empty
 } catch (e) {}
